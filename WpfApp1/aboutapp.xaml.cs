4de074e687b64116c899d2e9469476a07063949a
Johan5140 <Johan5140@local>
2024-03-16 15:52:14 +0100

    Add project files.

---
+++ WpfApp1/aboutapp.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Chemie
+{
+    /// <summary>
+    /// Interaction logic for aboutapp.xaml
+    /// </summary>
+    public partial class aboutapp : Window
+    {
+        public aboutapp()
+        {
+            InitializeComponent();
+            Nameapp.Content = "Chemie";
+            ver.Content = "0.2.0";
+            Autor.Content = "Copyright © Johan5140 (GitHub)  2024";
+            abouttext.Text = Soubory.Resource.info; 
+        }
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (!e.Handled && e.Key == Key.Escape && Keyboard.Modifiers == ModifierKeys.None)
+            {
+                this.Close();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ WpfApp1/infooprvku.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+//using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interakční logika pro infooprvku.xaml
+    /// </summary>
+    public partial class infooprvku : Window
+    {
+        public infooprvku(string kodprvku)
+        {
+            InitializeComponent();
+            var xml = XDocument.Parse(Soubory.Resource.prvky);
+            // Vytvoření datasetu
+            DataSet dataSet = new DataSet();
+            // Vložení dat do datasetu
+            dataSet.ReadXml(xml.CreateReader());
+            DataView dataView = new DataView(dataSet.Tables[kodprvku]);
+            dataGrid1.ItemsSource = dataView;
+            Headline.Content = "Informace o prvku: "+kodprvku;
+            PreviewKeyDown += (s, e) => { if (e.Key == Key.Escape) this.Close(); };
+        }
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (!e.Handled && e.Key == Key.Escape && Keyboard.Modifiers == ModifierKeys.None)
+            {
+                this.Close();
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
