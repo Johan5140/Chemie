2c4ce2edc3c16978d46df6adc474dfc3d030aa43
Johan5140 <kaavi.98@outlook.com>
2024-06-03 17:50:53 +0200

    Oprava

--- WpfApp1/Properties/Settings.Designer.cs
+++ WpfApp1/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace Chemie.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("#000000")]
+        [global::System.Configuration.DefaultSettingValueAttribute("#b9d1ea")]
         public string head {
             get {
                 return ((string)(this["head"]));
